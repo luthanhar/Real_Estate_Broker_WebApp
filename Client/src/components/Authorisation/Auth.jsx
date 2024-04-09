5e92fa406be8c5de9cc3a854267ee4f23db585c1
luthanhar <armeet.luthra70801@gmail.com>
2024-04-09 15:35:20 +0530

    Issue #5 resolved

--- Client/src/components/Authorisation/Auth.jsx
+++ Client/src/components/Authorisation/Auth.jsx
@@ -1,19 +1,31 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("userId") ?? false
+  );
   const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("userId");
+    if (token) {
+      setIsLoggedIn(true);
+      setUserId(token);
+    }
+  }, []);
 
   const login = (userId) => {
     setIsLoggedIn(true);
     setUserId(userId);
+    localStorage.setItem("userId", userId);
   };
 
   const logout = () => {
     setIsLoggedIn(false);
     setUserId(null);
+    localStorage.removeItem("userId");
   };
 
   return (
